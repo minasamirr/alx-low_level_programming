fe81305df08f34c7f3529c80077686a1859006a4
minasamirr <mina430q@gamil.com>
2023-09-07 06:47:58 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /*
- *  a C program that prints using puts
+ *   main - Entry point
+ *   Description: A C program that prints with put function
+ *   Return: Always 0 (Success)
  */
 int main(void)
 {
