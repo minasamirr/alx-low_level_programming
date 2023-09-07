b1ccd415274ec541b02d9f975e8da78e5a353b65
minasamirr <mina430q@gamil.com>
2023-09-07 07:27:28 -0700

    extra task 100,101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a quote using write function
+ *    write(int fd, const void *buf, size_t count);
+ *
+ * Return: This time we return an error 1.
+*/
+
+int main(void)
+{
+	char quotee[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quotee, 58);
+	return (1);
+}
