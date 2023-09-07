3325ef120bd908c34444ae820e3030196ac63663
minasamirr <mina430q@gamil.com>
2023-09-07 06:56:47 -0700

    task 4-6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,6 +7,7 @@
  *
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
