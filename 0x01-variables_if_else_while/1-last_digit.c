133addbdd169b1b6a3951ddf6f38bad9d8a62a2a
minasamirr <mina430q@gamil.com>
2023-09-08 04:40:26 -0700

    c variables if else while task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,5 +28,4 @@ int main(void)
 	else if ((last_digit < 6) && (last_digit != 0))
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
 
-	return (0);
-}
+	return (0);}
