0ccd90deed0cf2638cd2c5ba095b4c8a51a6744f
Mina Samir <73786470+minasamirr@users.noreply.github.com>
2023-09-08 16:06:50 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 	int digit1 = 0, digit2;
-	
 	for (digit1 = 0; digit1 < 100; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 100; digit2++)
@@ -29,4 +28,5 @@ int main(void)
 	}
 	putchar('\n');
 
-	return (0);}
+	return (0);
+}
