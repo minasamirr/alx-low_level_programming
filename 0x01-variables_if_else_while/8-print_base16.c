5b5be240451e77255d745e2f35daccb601d7dc74
minasamirr <mina430q@gamil.com>
2023-09-08 05:08:21 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbers of base16 in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+    char hex_digits[] = "0123456789abcdef";
+    int i;
+
+    for (i = 0; i < 16; i++)
+    {
+        putchar(hex_digits[i]);
+    }
+
+    putchar('\n');
+
+    return (0);
+}
