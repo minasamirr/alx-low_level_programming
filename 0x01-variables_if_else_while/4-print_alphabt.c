cbfd02bec76fe85ee979d462799de2b02c7e93d0
minasamirr <mina430q@gamil.com>
2023-09-08 04:53:22 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters except q and e
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (!(ch == 'e' || ch == 'q'))
+			putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
