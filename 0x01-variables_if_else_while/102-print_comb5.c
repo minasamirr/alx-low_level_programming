52bbf82bafb7eef0af56cb7eabd3bf73435c780f
minasamirr <mina430q@gamil.com>
2023-09-08 06:01:37 -0700

    ex task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+	
+	for (digit1 = 0; digit1 < 100; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 100; digit2++)
+		{
+			putchar((digit1 / 10) + 48);
+			putchar((digit1 % 10) + 48);
+			putchar(' ');
+			putchar((digit2 / 10) + 48);
+			putchar((digit2 % 10) + 48);
+			if ((digit1 != 98) || (digit2 != 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
