7e6246acfb655b82d18c6197f95f2916d5d8caa1
minasamirr <mina430q@gamil.com>
2023-09-08 05:32:19 -0700

    ex task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different
+ *           combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit1, digit2;
+
+	for(digit1 = 0; digit1 < 10; digit1++)
+	{
+		for(digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar(digit1 + 48);
+			putchar(digit2 + 48);
+			if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
