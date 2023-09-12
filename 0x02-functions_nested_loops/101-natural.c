160903daee1a83821c5c0c53426643783177bb89
minasamirr <mina430q@gamil.com>
2023-09-12 07:04:24 -0700

    Functions, nested loops extra task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: computes the sum of all the multiples 
+ * Description: computes the sum of all the multiples
  *              of 3 or 5 below 1024 (excluded)
  *
  * Return: Always 0 (Success)
