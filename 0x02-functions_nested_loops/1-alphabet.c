10b94ce0519b548765fc3fd2a62acd356a1ab113
minasamirr <mina430q@gamil.com>
2023-09-12 05:07:34 -0700

    Functions, nested loops task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include"main.h"
 
 /**
- *
- *
+ * print_alphabet - using the _putchar function to print
+ *                 the alphabet a - z
 */
 
 void print_alphabet(void)
 {
 	char chr;
 
-	for(chr = 'a'; chr < 'z'; chr++)
+	for (chr = 'a'; chr <= 'z'; chr++)
 	{
 		_putchar(chr);
 	}
