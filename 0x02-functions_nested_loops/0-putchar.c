9a3762118f1df40590dc0c2a8dedcae017770818
minasamirr <mina430q@gamil.com>
2023-09-12 04:58:57 -0700

    Functions, nested loops task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,9 +13,9 @@ int main(void)
 	char outp[] = "_putchar";
 	int i;
 
-	for(i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(outp[i]);
-	_putchar("\n");
+	_putchar('\n');
 
 	return (0);
 }
