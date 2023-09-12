cd724a58ffad6b49551e2751803a580e02a41574
minasamirr <mina430q@gamil.com>
2023-09-12 04:50:30 -0700

    Functions, nested loops task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char outp[] = "_putchar";
+	int i;
+
+	for(i = 0; i < 8; i++)
+		_putchar(outp[i]);
+	_putchar("\n");
+
+	return (0);
+}
