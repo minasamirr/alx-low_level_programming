1efc47339d50f4f901af0b10ed035d3a616d7301
minasamirr <mina430q@gamil.com>
2023-09-14 04:04:16 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,21 +9,20 @@
 
 void more_numbers(void)
 {
-	int i, j = 0, num;
-	
-	while (j < 10)
+	int num, row, count;
+
+	for (row = 0; row < 10; row++)
 	{
-		for (i = 0; i < 15; i++)
+		for (count = 0; count < 15; count++)
 		{
-			num = i;
-			if (i > 9)
+			num = count;
+			if (count > 9)
 			{
 				_putchar(1 + 48);
-				num = i % 10;
+				num = count % 10;
 			}
 			_putchar(num + 48);
 		}
 		_putchar('\n');
-		j++;
 	}
 }
