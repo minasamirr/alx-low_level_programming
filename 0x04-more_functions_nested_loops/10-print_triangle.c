86ce635e7c371125124e6903a98f712ebb2a454f
minasamirr <mina430q@gamil.com>
2023-09-14 05:22:59 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: size of the triangle
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_triangle(int size)
+{
+	int row, space, hash;
+
+	for (row = 0; row < size; row++)
+	{
+		for (space = size - 1; space > row; space--)
+			_putchar(' ');
+		for (hash = 0; hash <= row; hash++)
+			_putchar(35);
+		_putchar('\n');
+	}
+}
