ecac9c36e59b2a55ba3087ba0e19bf9c7d1dbdd4
minasamirr <mina430q@gamil.com>
2023-09-14 03:45:24 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-/*
- * print_most_numbers - print 0 - 9
- *                  Do not print 2 and 4
- *                  using _putchar only twice
+/**
+ * print_most_numbers - print 0 - 9 apart
+ *                 from 2 and 4 and you can
+ *                 only use _putchar twice
+ * Return: Always 0 (Success)
 */
 
 void print_most_numbers(void)
