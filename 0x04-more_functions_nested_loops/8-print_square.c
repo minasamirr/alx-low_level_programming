f9e64997de2a52e682a12b4e6b0421fe1b7748e9
minasamirr <mina430q@gamil.com>
2023-09-14 04:51:41 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,3 +1,13 @@
+#include"main.h"
+
+/**
+ * print_square - print a square using the character #
+ *
+ * @size: is the size of the square
+ *
+ * Return: Always 0 (Success)
+*/
+
 void print_square(int size)
 {
 	int row, col;
