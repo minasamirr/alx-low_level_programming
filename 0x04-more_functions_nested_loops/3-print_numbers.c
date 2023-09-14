e0bb91ef6d9698bbba67d2491d116a5f3cdb437f
minasamirr <mina430q@gamil.com>
2023-09-14 03:38:18 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/*
- * print_numbers - prints the numbers from 0 to 9
- * 			using _putchar only
+/**
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
  *
  * Return: Always 0 (Success)
 */
