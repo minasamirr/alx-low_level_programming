b41772c4779d01b9c6a17873fdffc1efde9982fa
minasamirr <mina430q@gamil.com>
2023-09-14 03:42:11 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * print_most_numbers - print 0 - 9
+ *                      Do not print 2 and 4
+ *                      using _putchar only twice
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (!(i == 2 || i == 4))
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
