0867cfb2825972494718328badea3a64cfe99501
minasamirr <mina430q@gamil.com>
2023-09-14 05:01:42 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,13 +17,13 @@ int main(void)
 	for (count = 1; count < 101; count++)
 	{
 		if ((count % 3 == 0) && (count % 5 == 0))
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else if (count %  3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (count % 5 == 0)
-			printf(Buzz);
+			printf("Buzz ");
 		else
-			printf("%d", count);
+			printf("%d ", count);
 	}
 	printf("\n");
 
