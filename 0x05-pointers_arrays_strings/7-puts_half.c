9e9a6bbb07778658b1a0eb69f23fd56ca7f9a595
minasamirr <mina430q@gamil.com>
2023-09-18 08:04:22 -0700

    Pointers, arrays and strings task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * 
+ * @str: char to check
+ *
+ * Return: 0
+ */
+ 
+void puts_half(char *str)
+{
+	int length = 0, n;
+
+	while (str[length] != '\0')
+		length++;
+	if (length + 1 % 2 != '0')
+		n = (length - 1) / 2;
+	else
+		n = (length / 2);
+	n++;
+
+	for (length = n; str[length] != '\0'; length++)
+	_putchar(str[length]);
+	_putchar('\n');
+}
