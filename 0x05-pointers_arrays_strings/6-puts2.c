ec5181074f6ba14ab975bbed42c93b0561433afe
minasamirr <mina430q@gamil.com>
2023-09-18 07:35:50 -0700

    Pointers, arrays and strings task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: char to check
+ *
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
