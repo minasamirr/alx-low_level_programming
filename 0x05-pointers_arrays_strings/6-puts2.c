ac39fedceb1a4208fb358c6688362d63ea3b1355
minasamirr <mina430q@gamil.com>
2023-09-18 07:45:03 -0700

    Pointers, arrays and strings task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,17 +5,15 @@
  *
  * @str: char to check
  *
- * Return: 0 is success
+ * Return: 0
  */
 
 void puts2(char *str)
 {
 	int i;
 
-	while (s[i] != '\0')
-		i++;
+	for (i = 0; str[i] != '\0'; i++)
 	if (i % 2 == 0)
 		_putchar(str[i]);
 	_putchar('\n');
 }
-
