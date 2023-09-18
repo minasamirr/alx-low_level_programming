c4973d97425e926c35bb8ba707b788ce48a17f1c
minasamirr <mina430q@gamil.com>
2023-09-18 06:56:58 -0700

    Pointers, arrays and strings task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _puts - prints a string, followed by a new line to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: 0
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
