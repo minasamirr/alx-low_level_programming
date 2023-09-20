d687dfad264f0b5a4e6a3e929f6177dd52e6c9d3
minasamirr <mina430q@gamil.com>
2023-09-20 09:39:21 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,8 +13,8 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	while (dest[i])
-		i++;
+	for (i = 0; dest[i] != '\0'; i++)
+		continue;
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i + j] = src[j];
