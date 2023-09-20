37077379088f49bc05dde454f539355a378ba90c
minasamirr <mina430q@gamil.com>
2023-09-20 11:17:06 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array
+ * @a:array
+ * @n:integer
+ * Return:void
+*/
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+	j = n-1;
+	for (i = 0; i < n / 2; i++, j--)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+}
