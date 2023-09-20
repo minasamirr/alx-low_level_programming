c74bb69824f1af73fdfd01bbc1ab54cc9db4ab04
minasamirr <mina430q@gamil.com>
2023-09-20 11:51:20 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of str
+ */
+char *cap_string(char *str)
+{
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(str + i))
+	{
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		{
+			if (i == 0)
+				*(str + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(str + i - 1))
+						*(str + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (str);
+}
