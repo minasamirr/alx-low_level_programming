4d3cd9296d28ef7eb2d44e9d65604955755b561b
minasamirr <mina430q@gamil.com>
2023-09-21 11:29:02 -0700

    fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/**
-  * print_number - Print any integer with putchar
-  * @n: Number to print
-  *
-  * Return: Nothing
-  */
+/*
+ * print_number - Prints any integer with putchar
+ * @n: Number to prints
+ *
+ * Return: Nothing
+*/
 void print_number(int n)
 {
 	if (n < 0)
