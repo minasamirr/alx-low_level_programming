2e097a276b3d87176d7f5dac69d16be683912d58
minasamirr <mina430q@gamil.com>
2023-09-21 10:48:05 -0700

    extra task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,14 @@
   */
 void print_number(int n)
 {
-
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n *= -1;
 	}
-
 
 	if (n / 10)
 		print_number(n / 10);
 
-	putchar(n % 10 + '0');
+	_putchar(n % 10 + '0');
 }
