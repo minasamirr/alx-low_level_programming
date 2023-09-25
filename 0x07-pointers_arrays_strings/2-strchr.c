ee92d28b1617a7515d77b8a3facec576cc4a302f
minasamirr <mina430q@gamil.com>
2023-09-25 05:54:22 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * @s: Pointer to the string to search in.
  * @c: The character to locate.
  *
- * Return: A pointer to the first occurrence of the character c in the string s,
+ * Return: A pointer to the first occurrence of the character c in the string s
  *         or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
