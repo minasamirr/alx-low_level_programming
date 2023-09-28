c6327d426bec72a5f7f4f766cd0db2c6e868663b
minasamirr <mina430q@gamil.com>
2023-09-28 10:27:11 -0700

    task 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,36 +2,24 @@
 
 /**
  * wildcmp_recursive - Helper function to compare two strings with '*'.
- * @s1: The first string to compare.
- * @s2: The second string to compare, which may contain '*'.
+ * @s1: Pointer to the first string.
+ * @s2: Pointer to the second string.
  * Return: 1 if the strings can be considered identical, otherwise return 0.
  */
 int wildcmp_recursive(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
-	{
 		return (1);
-	}
 	if (*s2 == '*')
 	{
-		while (*(s2 + 1) == '*')
+		if (*s1 == '\0')
 		{
-			s2++;
+			return (wildcmp_recursive(s1, s2 + 1));
 		}
-		while (*s1 != '\0')
-		{
-			if (wildcmp_recursive(s1, s2 + 1))
-			{
-				return (1);
-			}
-			s1++;
-		}
-		return (wildcmp_recursive(s1, s2 + 1));
+		return (wildcmp_recursive(s1 + 1, s2) || wildcmp_recursive(s1, s2 + 1));
 	}
 	if (*s1 == *s2 || (*s2 == '?' && *s1 != '\0'))
-	{
 		return (wildcmp_recursive(s1 + 1, s2 + 1));
-	}
 	return (0);
 }
 
