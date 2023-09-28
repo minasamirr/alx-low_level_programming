8a28e0f32f9ff0d1ea2bac7f32881ecd43c3371a
minasamirr <mina430q@gamil.com>
2023-09-28 08:37:06 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "mina.h"
+
+/**
+ * _pow_recursion - Calculates the value of x raised to the power of y.
+ * @x: The base.
+ * @y: The exponent.
+ * Return: The result of x^y, or -1 if y is negative.
+*/
+int _pow_recursion(int x, int y)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+
+}
