cfcf050bccf8ab7986174e4e4d9c799c3a9b20ff
minasamirr <mina430q@gamil.com>
2023-09-28 10:15:55 -0700

    task 101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * wildcmp_recursive - Helper function to compare two strings with '*'.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare, which may contain '*'.
+ * Return: 1 if the strings can be considered identical, otherwise return 0.
+ */
+int wildcmp_recursive(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		while (*(s2 + 1) == '*')
+		{
+			s2++;
+		}
+		while (*s1 != '\0')
+		{
+			if (wildcmp_recursive(s1, s2 + 1))
+			{
+				return (1);
+			}
+			s1++;
+		}
+		return (wildcmp_recursive(s1, s2 + 1));
+	}
+	if (*s1 == *s2 || (*s2 == '?' && *s1 != '\0'))
+	{
+		return (wildcmp_recursive(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
+
+/**
+ * wildcmp - Compares two strings with '*' as a wildcard.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare, which may contain '*'.
+ * Return: 1 if the strings can be considered identical, otherwise return 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (wildcmp_recursive(s1, s2));
+}
