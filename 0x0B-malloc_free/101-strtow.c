07811172a010a4c92239c827b6d533733701815e
minasamirr <1701538@eng.asu.edu.eg>
2023-10-04 15:41:27 -0700

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,100 +1,55 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
-/**
- * count_words - Counts the number of words in a string.
- * @str: The input string.
- *
- * Return: The count of words in the string.
- */
 int count_words(char *str);
+void create_words(char **words, char *str, int num_words);
+int is_whitespace(char c);
 
 /**
  * strtow - Splits a string into words.
  * @str: The input string.
  *
- * Return: A pointer to an array of strings (words).
- *         Each element of the array contains a single word, null-terminated.
- *         The last element of the returned array is set to NULL.
- *         Returns NULL if str == NULL or str == "".
- *         Returns NULL if memory allocation fails.
- */
-char **strtow(char *str);
-
-/**
- * is_space - Checks if a character is a whitespace character.
- * @c: The character to check.
- *
- * Return: 1 if c is a whitespace character, 0 otherwise.
- */
-int is_space(char c);
-
-/**
- * strtow - splits a string into words.
- * @str: the string
- *
- * Return: returns a pointer to an array of strings (words)
+ * Return: A pointer to an array of strings (words), or NULL on failure.
+ * Each element of the array contains a single word, null-terminated.
+ * The last element of the array is NULL.
  */
 char **strtow(char *str)
 {
-	/* Check for NULL or empty string */
+	int num_words, i = 0, in_word = 0, start = 0, word_index = 0;
+	char **words;
+
 	if (str == NULL || *str == '\0')
 		return (NULL);
 
-	/* Count the number of words in the input string */
-	int word_count = count_words(str);
-
-	if (word_count == 0)
+	num_words = count_words(str);
+	if (num_words == 0)
 		return (NULL);
 
-	/* Allocate memory for an array of strings (words) */
-	char **words = (char **)malloc((word_count + 1) * sizeof(char *));
-
+	words = (char **)malloc((num_words + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
 
-	int word_index = 0;
-	int start = 0;
-	int end = 0;
-
-	/* Split the string into words and store them in the array */
-	while (str[start] != '\0')
+	while (str[i])
 	{
-		/* Skip leading whitespace */
-		while (is_space(str[start]))
-			start++;
-		end = start;
-
-		/* Find the end of the current word */
-		while (str[end] != '\0' && !is_space(str[end]))
-			end++;
-
-		int word_length = end - start;
-
-		/* Allocate memory for the word and copy it */
-		words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-		if (words[word_index] == NULL)
+		if (!in_word && !is_whitespace(str[i]))
 		{
-			/* Free memory and return NULL if allocation fails */
-			for (int i = 0; i < word_index; i++)
-				free(words[i]);
-			free(words);
-			return (NULL);
+			start = i;
+			in_word = 1;
 		}
-
-		/* Copy the word */
-		for (int i = 0; i < word_length; i++)
-			words[word_index][i] = str[start + i];
-
-		words[word_index][word_length] = '\0';
-
-		start = end;
-		word_index++;
+		else if (in_word && is_whitespace(str[i]))
+		{
+			in_word = 0;
+			create_words(words, str + start, i - start);
+			word_index++;
+		}
+		i++;
 	}
 
-	/* Set the last element of the array to NULL */
+	if (in_word)
+		create_words(words, str + start, i - start);
+
 	words[word_index] = NULL;
+
 	return (words);
 }
 
@@ -102,37 +57,54 @@ char **strtow(char *str)
  * count_words - Counts the number of words in a string.
  * @str: The input string.
  *
- * Return: The count of words in the string.
+ * Return: The number of words.
  */
 int count_words(char *str)
 {
-	int count = 0;
-	int i = 0;
+	int count = 0, in_word = 0;
 
-	while (str[i] != '\0')
+	while (*str)
 	{
-		if (!is_space(str[i]))
+		if (is_whitespace(*str))
 		{
+			in_word = 0;
+		}
+		else if (!in_word)
+		{
+			in_word = 1;
 			count++;
-			while (str[i] != '\0' && !is_space(str[i]))
-				i++;
 		}
-		else
-		{
-			i++;
-		}
+		str++;
 	}
-
 	return (count);
 }
 
 /**
- * is_space - Checks if a character is a whitespace character.
+ * create_words - Creates a word and inserts it into the array.
+ * @words: The array of strings.
+ * @str: The string containing the word.
+ * @length: The length of the word.
+ */
+void create_words(char **words, char *str, int length)
+{
+	words[0] = (char *)malloc((length + 1) * sizeof(char));
+	if (words[0] == NULL)
+		return;
+
+	for (int i = 0; i < length; i++)
+	{
+		words[0][i] = str[i];
+	}
+	words[0][length] = '\0';
+}
+
+/**
+ * is_whitespace - Checks if a character is whitespace.
  * @c: The character to check.
  *
- * Return: 1 if c is a whitespace character, 0 otherwise.
+ * Return: 1 if c is whitespace, 0 otherwise.
  */
-int is_space(char c)
+int is_whitespace(char c)
 {
-	return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+	return (c == ' ' || c == '\t' || c == '\n');
 }
