e2d27980b172030cab4e716c578ba5feecb2b5e7
minasamirr <1701538@eng.asu.edu.eg>
2023-10-07 03:15:08 -0700

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,15 +20,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (ptr == NULL)
+	{
 		return (malloc(new_size));
+	}
 
 	if (new_size == old_size)
+	{
 		return (ptr);
+	}
 
 	new_ptr = malloc(new_size);
 
 	if (new_ptr == NULL)
+	{
 		return (NULL);
+	}
 
 	if (new_size > old_size)
 	{
