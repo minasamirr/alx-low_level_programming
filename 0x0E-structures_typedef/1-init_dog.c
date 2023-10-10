9c73efb80fc5b281a5c9bf004f181aecb2ae8adc
minasamirr <1701538@eng.asu.edu.eg>
2023-10-10 12:17:41 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,8 @@
  * @age: Age of the dog.
  * @owner: Owner of the dog.
  *
- * Description: This function initializes a struct dog with the provided values.
+ * Description: This function initializes
+ * a struct dog with the provided values.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
