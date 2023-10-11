759cf65ea7be1fa98a562cae983d8443559cd433
minasamirr <1701538@eng.asu.edu.eg>
2023-10-11 10:13:28 -0700

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,10 +8,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
+
 	if (array == NULL || action == NULL)
 		return;
-
-	size_t i;
 
 	for (i = 0; i < size; i++)
 	{
