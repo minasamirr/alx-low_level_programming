f0eef0bbfd3418c97c58e550456ea141161ec35f
minasamirr <1701538@eng.asu.edu.eg>
2023-10-11 11:48:03 -0700

    task 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_opcodes - Print opcodes of the main function.
+ * @num_bytes: Number of bytes to print.
+ */
+void print_opcodes(int num_bytes)
+{
+	char *ptr = (char *)main;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", ptr[i] & 0xFF);
+		if (i != num_bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry point.
+ *
+ * Usage: ./main number_of_bytes
+ *
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	print_opcodes(num_bytes);
+
+	return (0);
+}
