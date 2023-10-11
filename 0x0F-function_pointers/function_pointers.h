e5c1b393db953f563ccc374b7fd5f0a5655258c1
minasamirr <1701538@eng.asu.edu.eg>
2023-10-11 09:54:35 -0700

    fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,13 @@
-#ifndef FUNCTION_POINTERS_
-#define FUNCTION_POINTERS_
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
 
 #include <stddef.h>
 #include <stdio.h>
 
+void print_name(char *name, void (*f)(char *));
+
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 int int_index(int *array, int size, int (*cmp)(int));
-
-void print_name(char *name, void (*f)(char *));
 
 #endif
