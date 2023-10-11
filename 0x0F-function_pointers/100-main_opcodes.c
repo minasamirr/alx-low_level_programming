82dc5561d6e63d59ff0dce7ca2c89598ba6a9239
minasamirr <1701538@eng.asu.edu.eg>
2023-10-11 11:59:52 -0700

    task 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,47 +3,42 @@
 #include <string.h>
 
 /**
- * print_opcodes - Print opcodes of the main function.
- * @num_bytes: Number of bytes to print.
- */
-void print_opcodes(int num_bytes)
-{
-	char *ptr = (char *)main;
-
-	for (int i = 0; i < num_bytes; i++)
-	{
-		printf("%02x", ptr[i] & 0xFF);
-		if (i != num_bytes - 1)
-			printf(" ");
-	}
-	printf("\n");
-}
-
-/**
- * main - Entry point.
+ * main - Entry point
  *
+ * Description: A program that prints the opcodes.
  * Usage: ./main number_of_bytes
  *
- * @argc: Argument count.
- * @argv: Argument vector.
- * Return: 0 on success.
- */
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0
+*/
+
 int main(int argc, char *argv[])
 {
+	int i;
+	int count;
+	char *ptr = (char *) main;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
 
-	int num_bytes = atoi(argv[1]);
-	if (num_bytes < 0)
+	count = atoi(argv[1]);
+	if (count < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	print_opcodes(num_bytes);
-
+	for (i = 0; i < count; i++)
+	{
+		printf("%02x", ptr[i] & 0xFF);
+		if (i != --count)
+			printf(" ");
+	}
+	printf("\n");
 	return (0);
 }
