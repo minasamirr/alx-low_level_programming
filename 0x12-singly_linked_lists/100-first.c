9c9c2025b4a79404b8a1799227c58eefb6bef58a
minasamirr <1701538@eng.asu.edu.eg>
2023-10-19 09:58:14 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * my_startup_function - Prints a message before the main function is executed.
+ */
+void my_startup_function(void) __attribute__((constructor));
+
+void my_startup_function(void) {
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
