03491f5107f3eed2c8df9946032aec4ef43d302f
minasamirr <1701538@eng.asu.edu.eg>
2023-10-19 10:01:17 -0700

    task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,8 @@
  */
 void my_startup_function(void) __attribute__((constructor));
 
-void my_startup_function(void) {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+void my_startup_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
