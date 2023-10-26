49fdc523823fc36a84717c04ddbb88fe7db4f2bc
minasamirr <1701538@eng.asu.edu.eg>
2023-10-26 15:32:23 -0700

    fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,15 +11,14 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
 
-	if (index >= (sizeof(unsigned long int) * 8 || n == NULL))
+	if (index >= (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
 	}
 
 	mask = 1UL << index;
-	mask = ~mask;
 
-	*n = *n & mask;
+	*n = *n & (~mask);
 
 	return (1);
 }
