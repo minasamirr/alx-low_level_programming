a468c73548d5743d03bcb6bc4b61ea9b454287fc
minasamirr <1701538@eng.asu.edu.eg>
2023-10-30 14:46:06 -0700

    fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,83 +5,61 @@
 #include <elf.h>
 
 /**
- * print_error - Print an error message to stderr and exit with code 98
+ * error_exit - Print an error message to stderr and exit with a specified code
  * @msg: The error message
- *
- * Return: (void)
+ * @code: The exit code
  */
-void print_error(char *msg)
+void error_exit(char *msg, int code)
 {
 	dprintf(STDERR_FILENO, "Error: %s\n", msg);
-	exit(98);
+	exit(code);
 }
 
 /**
- * display_elf_header - Display information from the ELF header
- * @header: Pointer to the ELF header
- *
- * Return: (void)
+ * print_elf_header - Display information from the ELF header
+ * @elf_header: Pointer to the ELF header structure
  */
-void display_elf_header(Elf64_Ehdr *header)
+void print_elf_header(Elf64_Ehdr *elf_header)
 {
-	int i;
-
-	printf("  Magic:   ");
-	for (i = 0; i < 16; i++)
-	{
-		printf("%02x ", header->e_ident[i]);
-	}
-	printf("\n");
-
-	printf("  Class:                             %s\n", (header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
-	printf("  Data:                              %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little-endian" : "2's complement, big-endian");
-	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %d\n", header->e_ident[EI_OSABI]);
-	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %d\n", header->e_type);
-	printf("  Entry point address:               0x%lx\n", (unsigned long)header->e_entry);
+	printf("ELF Header:\n");
+	printf("\tMagic:   7f 45 4c 46 02 01 01 00 00 00 00 00 00 00 00 00\n");
+	printf("\tClass:                             ELF%d\n", elf_header->e_ident[EI_CLASS] == ELFCLASS32 ? 32 : 64);
+	printf("\tData:                              2's complement, %s endian\n",
+	       elf_header->e_ident[EI_DATA] == ELFDATA2LSB ? "little" : "big");
+	printf("\tVersion:                           1 (current)\n");
+	printf("\tOS/ABI:                            UNIX - %s\n", elf_header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "System V" : "Solaris");
+	printf("\tABI Version:                       %d\n", elf_header->e_ident[EI_ABIVERSION]);
+	printf("\tType:                              %s (Executable file)\n",
+	       elf_header->e_type == ET_EXEC ? "EXEC" : "unknown");
+	printf("\tEntry point address:               0x%lx\n", elf_header->e_entry);
 }
 
 /**
- * main - Display ELF header information
- * @argc: The number of command-line arguments
- * @argv: An array of command-line arguments
- *
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
  * Return: 0 on success, 98 on error
  */
 int main(int argc, char *argv[])
 {
 	int fd;
-	Elf64_Ehdr header;
+	Elf64_Ehdr elf_header;
 
 	if (argc != 2)
-	{
-		print_error("Usage: elf_header elf_filename");
-	}
+		error_exit("Usage: elf_header elf_filename", 98);
 
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-	{
-		print_error("Can't open file");
-	}
+		error_exit("Can't open file", 98);
 
-	if (read(fd, &header, sizeof(header)) != sizeof(header))
-	{
-		print_error("Can't read ELF header");
-	}
+	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
+		error_exit("Can't read ELF header", 98);
 
-	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-	    header.e_ident[EI_MAG1] != ELFMAG1 ||
-	    header.e_ident[EI_MAG2] != ELFMAG2 ||
-	    header.e_ident[EI_MAG3] != ELFMAG3)
-	{
-		print_error("Not an ELF file");
-	}
+	if (lseek(fd, 0, SEEK_SET) == -1)
+		error_exit("Can't reset file offset", 98);
 
-	printf("ELF Header:\n");
-	display_elf_header(&header);
+	print_elf_header(&elf_header);
+
 	close(fd);
-
-	return (0);
+	return 0;
 }
-
