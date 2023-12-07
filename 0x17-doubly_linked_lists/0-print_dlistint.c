3a530adf02b4b39d1e650a19e19a4dce092883be
minasamirr <1701538@eng.asu.edu.eg>
2023-12-07 11:23:17 -0800

    task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,10 +8,12 @@
  *
  * Return: The number of nodes in the list.
  */
-size_t print_dlistint(const dlistint_t *h) {
+size_t print_dlistint(const dlistint_t *h)
+{
 	size_t node_count = 0;
 
-	while (h != NULL) {
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
 		h = h->next;
 		node_count++;
