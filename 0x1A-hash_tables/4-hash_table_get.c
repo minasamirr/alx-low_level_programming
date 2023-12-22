f06a612b9ed9871d5c9a73241a67dffffc93a43a
minasamirr <1701538@eng.asu.edu.eg>
2023-12-22 10:36:19 -0800

    task

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,20 +10,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *current;
+
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-
-	hash_node_t *current = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
 
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
-			return (current->value);
+		return (current->value);
 
 		current = current->next;
 	}
-
 	return (NULL);
 }
