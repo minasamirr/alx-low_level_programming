d33240dd81a6b85dd58f3a52e4a8598d57c5a647
minasamirr <1701538@eng.asu.edu.eg>
2023-12-22 10:27:21 -0800

    task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,32 +11,37 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
+    unsigned long int index;
+    hash_node_t *new_node;
 
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+    if (ht == NULL || key == NULL || *key == '\0')
+        return (0);
 
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+    index = key_index((const unsigned char *)key, ht->size);
 
-	if (new_node == NULL)
-		return (0);
+    new_node = malloc(sizeof(hash_node_t));
 
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
+    if (new_node == NULL)
+        return (0);
 
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+    new_node->key = strdup(key);
+    if (new_node->key == NULL)
+    {
+        free(new_node);
+        return (0);
+    }
 
-	return (1);
+    new_node->value = strdup(value);
+    if (new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node);
+        return (0);
+    }
+
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+
+    return (1);
 }
+
