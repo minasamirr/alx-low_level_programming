5f1683b7d7f8d0f83f5d233cdca31878c3403753
minasamirr <1701538@eng.asu.edu.eg>
2024-02-22 06:28:42 -0800

    task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    island_perimeter function
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
